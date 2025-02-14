d4a97d4096a7216227f84b5ead5353d47a7d6f53
Student <andreevdamir266@gmail.com>
2025-02-14 11:57:24 +0300

    commit1

---
+++ DBManager.py
@@ -0,0 +1,5 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost", database="prostoy", user="postgres", password="damir_999"
+)
--- mail/admin.py
+++ mail/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
 
-# Register your models here.
-#import psycopg2
-
-#conn = psycopg2.connect(
-    #host="localhost", database="prostoy", user="postgres", password="damir_999"
-#)
