0458283511805be0f2e8a420f5af479ea6f61851
Student <andreevdamir266@gmail.com>
2025-03-03 15:24:38 +0300

    commit7

--- clients/urls.py
+++ clients/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from .views import ClientsListView, ClientsCreateView, ClientsUpdateView, ClientsDeleteView, ClientsDetailView
 
 urlpatterns = [
-    path('clients/', ClientsListView.as_view(), name='clients_list'),
+    path('clients/', cache_page(60)(ClientsListView.as_view()), name='clients_list'),
     path('create/', ClientsCreateView.as_view(), name='clients_create'),
     path('<int:pk>/', ClientsDetailView.as_view(), name='clients_detail'),
     path('<int:pk>/edit/', ClientsUpdateView.as_view(), name='clients_edit'),
--- mailings/urls.py
+++ mailings/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from .views import MessageListView, MessageCreateView, MessageUpdateView, MessageDeleteView, MessageDetailView, \
     MailingListView, MailingCreateView, MailingUpdateView, MailingDetailView, MailingDeleteView
 
 
 urlpatterns = [
-    path('messages/', MessageListView.as_view(), name='message_list'),
+    path('messages/', cache_page(60)(MessageListView.as_view()), name='message_list'),
     path('messages/create/', MessageCreateView.as_view(), name='message_create'),
     path('messages/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
     path('messages/<int:pk>/edit/', MessageUpdateView.as_view(), name='message_edit'),
     path('messages/<int:pk>/delete/', MessageDeleteView.as_view(), name='message_delete'),
 
-    path('mailings/', MailingListView.as_view(), name='mailing_list'),
+    path('mailings/', cache_page(60)(MailingListView.as_view()), name='mailing_list'),
     path('mailings/create/', MailingCreateView.as_view(), name='mailing_create'),
     path('mailings/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
     path('mailings/<int:pk>/edit/', MailingUpdateView.as_view(), name='mailing_edit'),
